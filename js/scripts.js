a08ccd8d9f498f6a05ee983a17030379a1484f0d
G0blinC0de <brewbrotherred@gmail.com>
2023-11-28 17:30:37 -0800

    add class materials and other stuff I don't recall from this morning

--- js/scripts.js
+++ js/scripts.js
@@ -30,28 +30,45 @@ AddressBook.prototype.deleteContact = function (id) {
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, persEmail, workEmail) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
+    this.persEmail = persEmail;
+    this.workEmail = workEmail;
 }
 
 Contact.prototype.fullName = function () {
     return this.firstName + " " + this.lastName;
 };
 
-
 // User Interface Logic ---------
 let addressBook = new AddressBook();
+
+function listContacts(addressBookToDisplay) {
+    let contactsDiv = document.querySelector("div#contacts");
+    contactsDiv.innerText = null;
+    const ul = document.createElement("ul");
+    Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
+        const contact = addressBookToDisplay.findContact(key);
+        const li = document.createElement("li");
+        li.append(contact.fullName() + ": " + contact.phoneNumber + ", " + contact.persEmail + ", " + contact.workEmail);
+        li.setAttribute("id", contact.id);
+        ul.append(li);
+    });
+    contactsDiv.append(ul);
+}
 
 function handleFormSubmission(event) {
     event.preventDefault();
     const inputtedFirstName = document.querySelector("input#new-first-name").value;
     const inputtedLastName = document.querySelector("input#new-last-name").value;
     const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    const inputtedPersEmail = document.querySelector("input#new-persEmail").value;
+    const inputtedworkEmail = document.querySelector("input#new-workEmail").value;
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedPersEmail, inputtedworkEmail);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    listContacts(addressBook);
 }
 
 window.addEventListener("load", function () {
